2d6be5ee65c68fff806bca755577245a1d1188d2
Administrator <Administrator@4115-006>
2024-02-29 21:04:21 +0200

    Add project files.

---
+++ targil3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace targil3
+{
+    class Program
+    {
+        public static void targil3() { }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DemoObject.demo();
+        }
+    }
+}
---
+++ targil3/demo obj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace targil3
+{
+    public class DemoObject
+    {
+        public static void demo()
+        {
+            Console.WriteLine("objects Demo");
+            Room r1 = new Room();
+            Console.WriteLine("chir in color = " + r1.char1.color );
+            r1.char1.color = "red";
+            Console.WriteLine("chir in color = " + r1.char1.color);
+
+
+        }
+    }
+    public class Chair
+    {
+        public string color = "blue";
+        public int high = 40;
+        public int lenge = 45;
+        }
+    public class Table
+    {
+        public string color = "red";
+        public int high = 75;
+        public int lenge = 80;
+    }
+    public class Room
+    {
+        public Chair char1 = new Chair();
+        public Table table1 = new Table();
+    }
+}
+
